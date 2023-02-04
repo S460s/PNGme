364a142509d5d695fde0a0f3da12940516213c34
s460 <66819532+S460s@users.noreply.github.com>
2023-02-04 15:22:14 +0200

    encode seems to work

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,51 @@ mod png;
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
+use std::fs::File;
+use std::io::Write;
+use std::str::FromStr;
+
 use args::{Commands, CLI};
 use clap::Parser;
+
+use crate::chunk::Chunk;
+use crate::chunk_type::ChunkType;
 
 fn main() -> Result<()> {
     let cli = CLI::parse();
 
     match &cli.command {
+        Commands::Encode {
+            message,
+            file_path,
+            chunk_type,
+        } => {
+            let chunk_type = ChunkType::from_str(chunk_type)?;
+            let chunk = Chunk::new(chunk_type, message.as_bytes().to_vec());
+
+            let mut file = File::options().append(true).open(file_path)?;
+            let res = file.write(chunk.as_bytes().as_ref())?;
+
+            println!("{res} bytes written to {file_path:?} successfully");
+        }
+
+        Commands::Decode {
+            file_path,
+            chunk_type,
+        } => {
+            todo!()
+        }
+
+        Commands::Remove {
+            file_path,
+            chunk_type,
+        } => {
+            todo!()
+        }
+
+        Commands::Print { file_path } => {
+            todo!()
+        }
         Commands::Banner => {
             println!(
                 "\n\n:::::::::  ::::    :::  ::::::::  ::::    ::::  :::::::::: 
@@ -25,8 +63,6 @@ fn main() -> Result<()> {
 ###        ###    ####  ########  ###       ### ########## \n\n"
             )
         }
-
-        _ => println!("There is no such command"),
     }
 
     Ok(())
